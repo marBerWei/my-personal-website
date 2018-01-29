60e5432a72a011b916decb8b15a7ac8b017c6c2c
martina weidenbaum <martina.weidenbaum@gmail.com>
2018-01-29 17:20:45 -0500

    changed more padding

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,8 +11,7 @@ class Nav extends React.Component {
         <li class="dropdown">
           <NavLink activeClassName="active" to="/stuff">Portfolio</NavLink>
           <div class="dropdown-content">
-			<NavLink activeClassName="active" to="/findMyFarmer">Find My Farmer</NavLink>
-			<NavLink activeClassName="active" to="/NYTimesArchiver">NY Times Archiver</NavLink>
+		
           </div>
         </li>
         <li class="dropdown"><NavLink activeClassName="active" to="/about">About Me</NavLink></li>
@@ -25,15 +24,17 @@ class Nav extends React.Component {
 
 export default Nav
 
-
-
-// <ul className="header">
-//               
-//             </ul>
-//             <div className="content">
-//               <Route exact path="/" component={Background}/>
-//               <Route path="/about" component={About}/>
-//               <Route path="/stuff" component={Stuff}/>
-//               <Route path="/contact" component={Contact}/>
-//               <Route path="/blog" component={Blog}/>
-//             </div>
+// <ul>
+//   <li class="dropdown">
+//     <NavLink activeClassName="active" to="/">Home</NavLink>
+//   </li>
+//   <li class="dropdown">
+//     <NavLink activeClassName="active" to="/stuff">Portfolio</NavLink>
+//     <div class="dropdown-content">
+// <NavLink activeClassName="active" to="/findMyFarmer">Find My Farmer</NavLink>
+// <NavLink activeClassName="active" to="/NYTimesArchiver">NY Times Archiver</NavLink>
+//     </div>
+//   </li>
+//   <li class="dropdown"><NavLink activeClassName="active" to="/about">About Me</NavLink></li>
+//   <li class="dropdown"><a href="https://martina147.typeform.com/to/tFPgmP" activeClassName="active">Contact</a></li>
+// </ul>
