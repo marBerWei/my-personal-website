3ebb4c20076868da559119953ab24461f8963ffb
martina weidenbaum <martina.weidenbaum@gmail.com>
2018-01-29 17:53:06 -0500

    added icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Nav from './Nav'
+import FaGithubSquare from 'react-icons/lib/fa/github-square';
+import FaFacebookOfficial from 'react-icons/lib/fa/facebook-official';
+import FaInstagram from 'react-icons/lib/fa/instagram';
 
 const Header = () => {
 	return(
@@ -8,7 +11,13 @@ const Header = () => {
 		<div className = "nameContainer">
 			<div className="name">
 				<div className="mainName"> Martina Weidenbaum</div>
-				<div className="subName"> Software Engineer</div>
+				<span><div className="subName"> 
+				Software Engineer
+				</div>
+					<a href="https://github.com/marBerWei" className="icons"><FaGithubSquare/></a>
+					<a href="https://www.facebook.com/martina.weidenbaum" className="icons"><FaFacebookOfficial/></a>
+					<a href="https://www.instagram.com/martina625/?hl=en" className="icons"><FaInstagram/></a>
+				</span>
 			</div>
 			<Nav/>
 		</div>
